a3717117c1266a35766835dcf93f3cff98ba2549
Blake Davis <58578456+davisblakep@users.noreply.github.com>
2021-05-18 00:03:28 -0400

    Testing push of dependencies and libraries

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def homePage():
-    return "<body style=background-color:black;height:100vh><div style=display:flex;justify-content:center;align-items:center><h1 style=color:white;>My first Python Web Page! Saving for Git Test 4</h1></div></body>"
+    return "<body style=background-color:black;height:100vh><div style=display:flex;justify-content:center;align-items:center><h1 style=color:white;>My first Python Web Page!</h1></div></body>"
